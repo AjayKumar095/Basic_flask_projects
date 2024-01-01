f2847aecd1c7447d7661e3e873d0d293a968de67
XCoode <jaykumar002kori@gmail.com>
2024-01-01 21:06:29 +0530

    working on module11

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
+import logging
 
 from modules.module1 import module1_bp
 from modules.module2 import module2_bp
@@ -6,11 +8,14 @@ from modules.module3 import module3_bp
 from modules.module4 import module4_bp
 from modules.module5 import module5_bp
 from modules.module6 import module6_bp
-from modules.module11 import module11_bp
+from modules.module11 import module11_bp, socketio
 
 app = Flask(__name__)
+logging.basicConfig(filename='static/app.log', level=logging.DEBUG)
 app.secret_key = 'fjjkf857fdjft548jnvfKJJNjhdfd'
 app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
+app.config['SECRET_KEY']="djft548jnvfK"
+socketio=SocketIO(app)
 
 app.register_blueprint(module1_bp, url_prefix='/module1')
 app.register_blueprint(module2_bp, url_prefix='/module2')
@@ -25,5 +30,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.debug=True
+    socketio.init_app(app)
+    socketio.run(app,port=5000)
 
--- modules/module11.py
+++ modules/module11.py
@@ -1,8 +1,18 @@
 from flask import Blueprint as bp, render_template
+from flask_socketio import SocketIO,send
+import logging
 
 module11_bp = bp('module11', __name__)
+socketio = SocketIO()
+logger = logging.getLogger(__name__)
 
-@module11_bp.route('/task3', methods=['POST'])
+@module11_bp.route('/task11', methods=["POST"])
 def task11():
-    res="hello"
-    return render_template("module11_result.html", res=res)
+    return render_template("module11_result.html")
+
+@socketio.on('message')
+def handle_message(message):
+    logger.info(f"Checking for the received message: {message}")
+    print("Recived message"+message)
+    socketio.send(message, broadcast=True)
+    
